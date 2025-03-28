96b1c7fdef586b642bc7225e522c6d5c77d0f110
Nick <9609041+0niSec@users.noreply.github.com>
2025-03-28 18:01:30 -0400

    Refactor address search markup for clarity and z-index adjustment.
    Adjusted the layout and comments for better readability and updated the z-index to ensure proper display above the Leaflet map.

--- resources/views/livewire/address-search.blade.php
+++ resources/views/livewire/address-search.blade.php
@@ -1,8 +1,14 @@
-<div class="absolute rounded-md top-4 bottom-0 left-0 right-0 px-3 z-[1000]">
+{{-- The Leaflet map is set with a really high z-index so we need to be above it, hence the arbitrary number --}}
+<div class="absolute top-4 right-0 left-0 z-[400] block rounded-md px-3">
     <flux:field wire:ignore>
-        <flux:input id="address_search" name="address" wire:model="address" type="text"
-                    class="rounded-md"
-                    wire:keyup.debounce.500ms="search($event.target.value)"
-                    placeholder="1 Apple Park Way, Cupertino, CA 95014"/>
+        <flux:input
+            id="address_search"
+            name="address"
+            wire:model="address"
+            type="text"
+            class="rounded-md"
+            wire:keyup.debounce.500ms="search($event.target.value)"
+            placeholder="1 Apple Park Way, Cupertino, CA 95014"
+        />
     </flux:field>
 </div>
