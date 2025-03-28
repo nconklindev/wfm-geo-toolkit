83ae61892a0c1d4188bb3c891e51ca620a1d398e
Nick <9609041+0niSec@users.noreply.github.com>
2025-03-28 18:00:06 -0400

    Add routes for imports, exports, and location management
    Added import/export routes for Known Places, updated KnownPlaceController routes to include permissions, and introduced resource routes for managing locations.

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,60 @@
 <?php
 
 use App\Http\Controllers\KnownPlaceController;
+use App\Http\Controllers\LocationController;
+use App\Livewire\ExportKnownPlaces;
+use App\Livewire\ImportKnownPlaces;
 use App\Livewire\Settings\Appearance;
 use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
 use Illuminate\Support\Facades\Route;
 
+// Home
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
+// About
 Route::get('/about', function () {
     return view('about');
 })->name('about');
 
+// Dashboard
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
+// Known Places Import & Export
+Route::get('known-places/import', ImportKnownPlaces::class)
+    ->middleware(['auth', 'verified'])
+    ->name('known-places.import');
+
+Route::get('known-places/export', ExportKnownPlaces::class)
+    ->middleware(['auth', 'verified'])
+    ->name('known-places.export');
+
+// Known Places Resources
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('known-places', KnownPlaceController::class);
+    Route::resource('known-places', KnownPlaceController::class)->except(['destroy', 'edit']);
+
+    Route::get('known-places/{knownPlace}/edit',
+        [KnownPlaceController::class, 'edit'])->name('known-places.edit')->can('update', ['knownPlace', 'user']);
+
+    Route::delete('known-places/{knownPlace}',
+        [KnownPlaceController::class, 'destroy'])->name('known-places.destroy')->can('delete', ['knownPlace', 'user']);
 });
 
+Route::get('downloads/sample-known-places',
+    [KnownPlaceController::class, 'downloadSample'])->middleware([
+    'auth', 'verified'
+])->name('downloads.sample-known-places');
+
+// Locations
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::resource('locations', LocationController::class);
+});
+
+// User Settings
 Route::middleware(['auth'])->group(function () {
     Route::redirect('settings', 'settings/profile');
 
