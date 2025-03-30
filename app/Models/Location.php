c49ac7ab42ec4e4dbc43b1d7ccf83ef7fbaa9b96
Nick <9609041+0niSec@users.noreply.github.com>
2025-03-30 15:39:03 -0400

    Add knownPlaces relationship to Location model
    Introduced a BelongsToMany relationship between Location and KnownPlace models to establish connectivity.

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Location extends Model
 {
     use HasFactory;
+
+    public function knownPlaces(): BelongsToMany
+    {
+        return $this->belongsToMany(KnownPlace::class);
+    }
 }
