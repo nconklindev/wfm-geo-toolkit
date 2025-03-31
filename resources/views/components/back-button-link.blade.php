347831c48924f9fe836dc315e55009152c4dec8d
Nick <9609041+0niSec@users.noreply.github.com>
2025-03-31 00:14:36 -0400

    Add back button component and fix edit button link typo
    Introduced a reusable back button component and corrected a syntax error in the edit item button link by removing an extra parenthesis.

---
+++ resources/views/components/back-button-link.blade.php
@@ -0,0 +1,7 @@
+<flux:button
+    href="{{ back() }}"
+    icon="arrow-left"
+    {{ $attributes->class('font-medium tracking-wider uppercase transition') }}
+>
+    Back
+</flux:button>
--- resources/views/components/edit-item-button-link.blade.php
+++ resources/views/components/edit-item-button-link.blade.php
@@ -5,7 +5,7 @@
 <flux:button
     variant="filled"
     icon="pencil-square"
-    href="{{ route('known-places.edit', $knownPlace) }}')"
+    href="{{ route('known-places.edit', $knownPlace) }})"
     {{ $attributes->class(['cursor-pointer bg-blue-600! tracking-wider uppercase transition hover:bg-blue-700! dark:bg-blue-600 dark:hover:bg-blue-400!']) }}
 >
     Edit
