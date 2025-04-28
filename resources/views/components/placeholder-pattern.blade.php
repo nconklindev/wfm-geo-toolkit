5a3eb0217ea58628dddc51135bbb70a9957a5a08
Nick <9609041+0niSec@users.noreply.github.com>
2025-04-28 17:48:51 -0400

    Refactor and enhance placeholder-pattern component
    Updated SVG structure to use clearer semantics and customizable stroke color. Added inline documentation and improved readability of the code.

--- resources/views/components/placeholder-pattern.blade.php
+++ resources/views/components/placeholder-pattern.blade.php
@@ -1,12 +1,20 @@
 @props([
-    'id' => uniqid(),
+    'id' => uniqid('pattern-'),
 ])
 
-<svg {{ $attributes }} fill="none">
+{{--
+    Generates an SVG placeholder with a cross-hatch pattern.
+    The pattern lines inherit the text color of the element.
+    Example: <x-placeholder-pattern class="h-32 w-full text-zinc-400 dark:text-zinc-600" />
+--}}
+<svg {{ $attributes->merge(['fill' => 'none']) }}>
     <defs>
-        <pattern id="pattern-{{ $id }}" x="0" y="0" width="8" height="8" patternUnits="userSpaceOnUse">
-            <path d="M-1 5L5 -1M3 9L8.5 3.5" stroke-width="0.5"></path>
+        <pattern id="{{ $id }}" x="0" y="0" width="8" height="8" patternUnits="userSpaceOnUse">
+            {{-- Cross-hatch pattern lines: one diagonal top-left to bottom-right, one diagonal top-right to bottom-left --}}
+            <path d="M0 0L8 8M8 0L0 8" stroke-width="0.5" stroke="currentColor"></path>
+            {{-- Use stroke="currentColor" --}}
         </pattern>
     </defs>
-    <rect stroke="none" fill="url(#pattern-{{ $id }})" width="100%" height="100%"></rect>
+    {{-- Fill the rectangle with the defined pattern --}}
+    <rect stroke="none" fill="url(#{{ $id }})" width="100%" height="100%"></rect>
 </svg>
