503038f5ce92bb7d95bff3475a3bd4fd38fd5c04
nconklindev <190518646+nconklindev@users.noreply.github.com>
2025-06-16 19:34:43 -0400

    Merge pull request #13
    * Add wire:current and cursor-pointer class for usability

--- resources/views/components/settings/layout.blade.php
+++ resources/views/components/settings/layout.blade.php
@@ -1,11 +1,25 @@
 <div class="flex items-start max-md:flex-col">
     <div class="mr-10 w-full pb-4 md:w-[220px]">
         <flux:navlist>
-            <flux:navlist.item :href="route('settings.profile')" wire:navigate>{{ __('Profile') }}</flux:navlist.item>
-            <flux:navlist.item :href="route('settings.password')" wire:navigate>
+            <flux:navlist.item
+                :href="route('settings.profile')"
+                wire:navigate
+                :wire:current="request()->routeIs('settings.profile')"
+            >
+                {{ __('Profile') }}
+            </flux:navlist.item>
+            <flux:navlist.item
+                :href="route('settings.password')"
+                wire:navigate
+                :wire:current="request()->routeIs('settings.password')"
+            >
                 {{ __('Password') }}
             </flux:navlist.item>
-            <flux:navlist.item :href="route('settings.appearance')" wire:navigate>
+            <flux:navlist.item
+                :href="route('settings.appearance')"
+                wire:navigate
+                :wire:current="request()->routeIs('settings.appearance')"
+            >
                 {{ __('Appearance') }}
             </flux:navlist.item>
         </flux:navlist>
