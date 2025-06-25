ac656a09a58a8985ddf384371d32a3683f0cc080
nconklindev <190518646+nconklindev@users.noreply.github.com>
2025-06-25 14:40:06 -0400

    Add `type` and `locations` properties to Point class
    Introduced optional `type` and `locations` properties to the `Point` class. Updated related methods for handling these new attributes during object creation and data transformation.

--- app/Point.php
+++ app/Point.php
@@ -10,22 +10,25 @@ class Point implements Wireable
         public float $latitude,
         public float $longitude,
         public string $label,
+        public string $type = 'known_place',
+        public array $locations = [],
         public int $radius = 50,
         public int $accuracy = 100,
         public string $color = '#3b82f6',
-    ) {
-    }
+    ) {}
 
     public static function fromLivewire($value): Point
     {
         $latitude = $value['latitude'];
         $longitude = $value['longitude'];
         $label = $value['label'];
+        $type = $value['type'];
+        $locations = $value['locations'];
         $radius = $value['radius'];
         $accuracy = $value['accuracy'];
         $color = $value['color'];
 
-        return new static($latitude, $longitude, $label, $radius, $accuracy, $color);
+        return new static($latitude, $longitude, $label, $type, $locations, $radius, $accuracy, $color);
     }
 
     public function toLivewire(): array
@@ -34,6 +37,8 @@ class Point implements Wireable
             'latitude' => $this->latitude,
             'longitude' => $this->longitude,
             'label' => $this->label,
+            'type' => $this->type,
+            'locations' => $this->locations,
             'radius' => $this->radius,
             'accuracy' => $this->accuracy,
             'color' => $this->color,
